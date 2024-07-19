9e5bbdebd38e83e3b0650f07d159c6f05f8371d4
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-07-19 09:29:18 +0900

    update:管理画面：トップのアイコン変更

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,18 +3,18 @@
         <div class="flex flex-col justify-center items-center shadow-xl text-center border-4 border-solid border-black bg-white w-[40%] md:w-[30%]">
             <a href="{{route("ShowPostPage")}}" class="w-full h-full hover:bg-black hover:text-white p-3">
                 <div class="flex flex-col gap-2">
-                    <p><i class="fa-solid fa-gift text-4xl"></i></p>
+                    <p><i class="fa-solid fa-file text-4xl"></i></p>
                     <p class="font-bold text-sm md:text-base">投稿</p>
                 </div>
             </a>
         </div>
         <div class="flex flex-col justify-center items-center shadow-xl text-center border-4 border-solid border-black bg-white w-[40%] md:w-[30%]">
-{{--            <a href="{{route("ShowQuestion")}}" class="w-full h-full hover:bg-black hover:text-white p-3">--}}
+            <a href="{{route("ShowPostPage")}}" class="w-full h-full hover:bg-black hover:text-white p-3">
                 <div class="flex flex-col gap-2">
                     <p><i class="fa-solid fa-magnifying-glass text-4xl"></i></p>
-                    <p class="font-bold text-sm md:text-base">質問設定</p>
+                    <p class="font-bold text-sm md:text-base">カテゴリー</p>
                 </div>
-{{--            </a>--}}
+            </a>
         </div>
     </div>
 </x-app-layout>
