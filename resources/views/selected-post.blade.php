1537ce9221e521a3ef17337ca5a7197ae84b474d
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-07-22 19:42:19 +0900

    fix:ユーザー画面/pdf：<iframe>だと表示はされるがダウンロードアイコンなどが表示されないため<a>に修正

--- resources/views/selected-post.blade.php
+++ resources/views/selected-post.blade.php
@@ -1,14 +1,15 @@
 <x-template title="RehaBiridge">
     <div class="flex justify-center items-center my-8 md:my-0 md:h-screen">
-        <div class="bg-white rounded-2xl flex flex-col items-center pt-8 h-[90vh] w-[90%] md:w-[80%] md:relative">
-            <h1 class="text-2xl font-bold">{{$selectedPost["name"]}}</h1>
+        <div class="bg-white rounded-2xl flex flex-col items-center justify-center gap-8 md:gap-16 pt-8 h-[90vh] w-[90%] md:w-[80%] md:relative">
+            <h1 class="text-2xl font-bold md:absolute md:top-8">{{$selectedPost["name"]}}</h1>
             <div>
                 @if($selectedPost["pdf"]!=null)
                     <div class="flex gap-6">
-                        <div>
-                            <iframe src="{{ asset($selectedPost['pdf']) }}" width="100%" height="500px" style="border:none;">
-                                このブラウザでは PDF の表示に対応していません。
-                            </iframe>                        </div>
+{{--                        <div>--}}
+{{--                            <iframe src="{{ asset($selectedPost['pdf']) }}" width="100%" height="500px" style="border:none;">--}}
+{{--                                このブラウザでは PDF の表示に対応していません。--}}
+{{--                            </iframe>                        --}}
+{{--                        </div>--}}
                         <div>
                             <a href="{{asset($selectedPost["pdf"])}}" target="_blank" class="text-blue-600 hover:underline">PDFを表示</a>
                         </div>
