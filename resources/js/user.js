809b9eb762b9b768f48df2b2ec86fa5ea1074ab1
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-07-23 22:15:16 +0900

    add；ユーザー画面：ハンバーガーメニュー実装

--- resources/js/user.js
+++ resources/js/user.js
@@ -1,3 +1,6 @@
+const HAMBURGER_ICON = document.getElementById('hamburger_icon');
+
+
 // 投稿表示アニメーション
 document.addEventListener("DOMContentLoaded", function() {
     const observerOptions = {
@@ -23,3 +26,49 @@ document.addEventListener("DOMContentLoaded", function() {
         observer.observe(story);
     });
 });
+
+//サイドメニュー表示
+HAMBURGER_ICON.addEventListener('click', function() {
+    // 画面背景色グレーに
+    let screen_cover = document.getElementsByClassName("screen_cover")[0];
+    screen_cover.classList.add("popup-bg-cover");
+
+    // サイドメニュー表示
+    side_menu.classList.remove("side_menu_off")
+    side_menu.classList.add("side_menu_show")
+
+    // mainをスクロール不可に
+    main.classList.add("scroll_none")
+})
+
+let side_menu = document.getElementsByClassName("side_menu_off")[0];
+let main = document.getElementsByTagName("body")[0];
+
+
+
+// メニューのいずれかもしくは✗クリック時に画面グレー&サイドメニュー非表示
+let side_li = document.getElementsByClassName("side_li");
+for(let i= 0; i<side_li.length;i++){
+    side_li[i].addEventListener("click",function (e){
+        let screen_cover = document.getElementsByClassName("screen_cover")[0];
+        screen_cover.classList.remove("popup-bg-cover");
+
+        side_menu.classList.remove("side_menu_show")
+        side_menu.classList.add("side_menu_off")
+
+        main.classList.remove("scroll_none")
+    })
+}
+
+// サイドメニューの外をクリックしたらサイドメニュー閉じる
+document.addEventListener("click",function (e){
+    if((!e.target.closest('div.side_wrapper'))&& e.target!==hamburger) {
+        let screen_cover = document.getElementsByClassName("screen_cover")[0];
+        screen_cover.classList.remove("popup-bg-cover");
+
+        side_menu.classList.remove("side_menu_show")
+        side_menu.classList.add("side_menu_off")
+
+        main.classList.remove("scroll_none")
+    }
+})
