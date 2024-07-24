2ea828e44b60d1b43eac5fa2d50a9698b3c7b5c1
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-07-24 22:37:29 +0900

    add：テスト用

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,4 +5,5 @@
     <x-index-top :latestPost="$latestPost"></x-index-top>
     <x-index-nav :categories="$categories" :clear="$clear"/>
     <x-index-gallery :posts="$posts"></x-index-gallery>
+    <h1>eeeeeeeeeeeeeeeeeeee</h1>
 </x-template>
