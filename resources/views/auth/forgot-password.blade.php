b26ecab0edb6932fc5162252e6b0a3cb065d25cd
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-07-29 10:54:53 +0900

    fix：管理画面/forgot-password.blade.php：英語表記だったので日本語に変更

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('メールアドレスを入力してください。') }}
     </div>
 
     <!-- Session Status -->
@@ -16,9 +16,17 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+
+        <div class="my-4 text-xs text-gray-600">
+            {{ __('ご登録されているメールアドレスにリンクをお送り致します。') }}
+        </div>
+
+        <div class="flex items-center justify-between mt-4">
+            <a href="/login" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                {{ __('ログイン画面') }}
+            </a>
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('送信') }}
             </x-primary-button>
         </div>
     </form>
